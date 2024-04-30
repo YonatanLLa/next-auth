c60fd7b999f18e994121f4c0a83a16bb07846af4
yonatanlla <yonatanllanto174@email.com>
2024-04-30 15:42:02 -0500

    install

--- actions/register.ts
+++ actions/register.ts
@@ -1,17 +1,38 @@
-"use server"
+"use server";
 
-import * as z from "zod"
+import * as z from "zod";
 import { RegiterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { error } from "console";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegiterSchema>) => {
+  const validateFields = RegiterSchema.safeParse(values);
 
-    const validateFields = RegiterSchema.safeParse(values)
+  if (!validateFields.success) {
+    return {
+      error: "invalid filds!",
+    };
+  }
 
-    if (!validateFields.success) {
-        return {
-            error: "invalid filds!"
-        }
-    }
+  const { email, password, name } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    return {success: "Email sent! "}
-}
+  const existingUsuer = await getUserByEmail(email);
+  if (existingUsuer) {
+    return {
+      error: "Email alredy in use",
+    };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+  //
+  return { success: "Email sent! " };
+};
